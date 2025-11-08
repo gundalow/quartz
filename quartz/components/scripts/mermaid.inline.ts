7c9811bd8116a2d9cd26adadd8fc0f0856f8f034
John Barker <john@johnrbarker.com>
2025-11-08 14:45:08 +0000

    Update Mermaid library version to 12.0.0

--- quartz/components/scripts/mermaid.inline.ts
+++ quartz/components/scripts/mermaid.inline.ts
@@ -150,7 +150,7 @@ document.addEventListener("nav", async () => {
 
   mermaidImport ||= await import(
     // @ts-ignore
-    "https://cdnjs.cloudflare.com/ajax/libs/mermaid/11.4.0/mermaid.esm.min.mjs"
+    "https://cdnjs.cloudflare.com/ajax/libs/mermaid/12.0.0/mermaid.esm.min.mjs"
   )
   const mermaid = mermaidImport.default
 
